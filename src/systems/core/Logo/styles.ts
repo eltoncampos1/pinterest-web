16dadfef337b6c5f3b32ffb6a38c7a220c88ee20
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-23 12:28:38 -0300

    feat: create logo component

---
+++ src/systems/core/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.svg`
+  display: block;
+  width: 32px;
+  height: 32px;
+  fill: ${({ theme }) => theme.colors.red_primary};
+`;
