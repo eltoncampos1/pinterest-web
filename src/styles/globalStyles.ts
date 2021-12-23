1c0d2612078a010118faa7e5fd3c223db4f0dce9
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-23 12:26:32 -0300

    chore: add fonts

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,1 +1,31 @@
 import { createGlobalStyle } from "styled-components";
+
+export const GLobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+    body, html {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, "ＭＳ Ｐゴシック", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        text-size-adjust: none;
+        font-size: 100%;
+        height: 100%;
+        font-size: 16px;
+    }
+
+    body {
+        background: #fff;
+    }
+
+    @media(max-width: 800px) {
+      html: font-size: 15px;
+    }
+
+    @media(max-width: 500px) {
+      html {
+        font-size: 14px;
+      }
+    }
+`;
