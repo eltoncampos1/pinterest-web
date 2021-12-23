7ef43fa49ba60a53841492d7796b444db468a176
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-23 13:02:50 -0300

    chore: start home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../systems/Header";
+import * as S from "./styles";
+
+export const Home = () => {
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
+};
