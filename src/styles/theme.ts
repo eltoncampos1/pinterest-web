1a23a904db68c25c72ef94e6b49513c2b4f0097c
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-03 20:36:06 -0300

    chore: add some base colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,13 @@
 export default {
   colors: {
     red_primary: "#e60023",
-    black_primary: "#111111",
-    black_secondary: "#211922",
-    black_tertiary: "#333333",
+    black_100: "#111111",
+    black_200: "#211922",
+    black_300: "#333333",
     blue_facebook: "#1877F2",
     gray_100: "#efefef",
     gray_200: "#e1e1e1",
-    gray_300: "##767676",
+    gray_300: "#767676",
     gray_400: "#3c4043",
     green_secondary: "#618c7b",
     white_primary: "#FFFFFF",
