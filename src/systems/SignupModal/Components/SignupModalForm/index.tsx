432f063c848429b4ee5b31d57ac69d7bfcf4589a
eltoncampos1 <eltoncampos36@gmail.com>
2022-01-03 21:31:42 -0300

    feat: create signup modal form

---
+++ src/systems/SignupModal/Components/SignupModalForm/index.tsx
@@ -0,0 +1,64 @@
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import theme from "../../../../styles/theme";
+import { FormInput, Button } from "../../../core";
+import * as S from "./styles";
+
+const inputs = [
+  {
+    type: "text",
+    placeholder: "Create a password",
+    errorMessage: "Hmm...that doesn't look like an email address",
+  },
+  {
+    type: "password",
+    placeholder: "Email",
+  },
+  {
+    type: "number",
+    placeholder: "Age",
+    errorMessage: "Ops...looks like you're not old enough yet",
+  },
+];
+
+export const SignupModalForm = () => {
+  return (
+    <S.Form>
+      {inputs.map((input) => (
+        <FormInput
+          placeholder={input.placeholder}
+          type={input.type}
+          errorMessage={input.errorMessage}
+        />
+      ))}
+
+      <Button
+        buttonBg={theme.colors.red_primary}
+        color="white"
+        onClick={() => console.log("clicou")}
+      >
+        Continue
+      </Button>
+
+      <span>or</span>
+      <S.SocialLogin>
+        <Button
+          buttonBg={theme.colors.blue_facebook}
+          color="white"
+          onClick={() => console.log("clicou")}
+        >
+          <FaFacebook />
+          Continue with facebook
+        </Button>
+        <Button
+          buttonBg={theme.colors.white_primary}
+          color={theme.colors.gray_300}
+          onClick={() => console.log("clicou")}
+        >
+          <FcGoogle />
+          Continue with Google
+        </Button>
+      </S.SocialLogin>
+    </S.Form>
+  );
+};
---
+++ src/systems/SignupModal/Components/SignupModalForm/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const SocialLogin = styled.div`
+  button {
+    width: 100%;
+    font-size: 1rem;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 1rem;
+      font-size: 1.5rem;
+    }
+
+    :nth-child(2) {
+      font-weight: 500;
+      border: 1px solid ${({ theme }) => theme.colors.gray_200};
+      font-family: "Google Sans", arial, sans-serif;
+      letter-spacing: 0.25px;
+      font-size: 0.9rem;
+      color: ${({ theme }) => theme.colors.gray_400};
+      margin-top: 0.8rem;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  input {
+    margin-bottom: 0.4rem;
+  }
+
+  span {
+    margin: 1rem 0;
+    font-size: 0.9rem;
+    font-weight: 700;
+    color: ${({ theme }) => theme.colors.black_300};
+    text-transform: uppercase;
+  }
+
+  > button {
+    width: 100%;
+    font-size: 1rem;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
